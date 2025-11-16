c01b1df920cdb7029b1220f8695c922d5db34981
sen <github@sen.cx>
2025-11-15 22:22:04 -0500

    Optimization to previous commit
    We only need to convert the tiny sliver of the image containing t(SB) to
    1-bit, no reason to do the whole image. This will save a few CPU cycles.

--- page1.py
+++ page1.py
@@ -205,7 +205,7 @@ class Page1(Vistars):
         secs: int
         # during the 2025 ion season t(SB) gained a blue background
         # we convert to 1-bit to eliminate this and make the OCR work
-        tsb_str = ocr(self.image.convert(mode="1", dither=Image.Dither.NONE).crop(box))
+        tsb_str = ocr(self.image.crop(box).convert(mode="1", dither=Image.Dither.NONE))
         if len(tsb_str) == 8 and tsb_str.isascii():
             try:
                 hrs = int(tsb_str[0:2])
